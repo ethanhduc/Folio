6f1a079f96231094a66a7aff4f35202d9aec2ae3
Ethan H. Nguyen <ethanhducnguyen@gmail.com>
2026-04-13 15:51:15 -0500

    Portfolios

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -17,9 +17,23 @@ namespace api.Data
         }
         public DbSet<Stock> Stocks{ get; set; } // represents the Stocks table in the database
         public DbSet<Comment> Comments{ get; set; } // maps the Comments table in the database from the Comment model
+        public DbSet<Portfolio> Portfolios{ get; set; } // maps the Portfolios table in the database from the Portfolio model
         protected override void OnModelCreating(ModelBuilder builder) // when model is being created, configures the model and seed data
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserId, p.StockId })); //foreign key 
+
+            builder.Entity<Portfolio>()
+                .HasOne(u => u.AppUser)
+                .WithMany(u => u.Portfolios)
+                .HasForeignKey(p => p.AppUserId);
+            
+            builder.Entity<Portfolio>()
+                .HasOne(u => u.Stock)
+                .WithMany(u => u.Portfolios)
+                .HasForeignKey(p => p.StockId);
+            
             List<IdentityRole> roles = new List<IdentityRole>
             {
                 new IdentityRole
---
+++ api/Models/Portfolio.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    [Table("Portfolios")]
+    public class Portfolio
+    {
+        public int AppUserId { get; set; } //foreign key linking stock and user table
+        public int StockId { get; set; }
+        public AppUser AppUser { get; set; } //for dev
+        public Stock Stock { get; set; } //for dev
+    }
+}
